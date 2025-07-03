71fc9360d3223f8b1de4a7a1a4d5edc59498a964
ShoopDeWoop <116699852+Rikuron@users.noreply.github.com>
2025-07-03 18:08:25 +0800

    Added Footer section

---
+++ src/app/sections/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="text-white py-6">
+      <div className="container mx-auto px-4">
+        <h3 className="text-lg mx-auto text-center">Copyright © 2025 <span className="font-bold">Yamz Computer Solutions</span></h3>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
